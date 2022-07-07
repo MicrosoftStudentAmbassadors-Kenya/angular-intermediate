d683f1a0b0b12d5979d29c4e8786378996be47e3
Toxic254 <cyrus_g@outlook.com>
2022-07-08 00:15:38 +0300

    Added pipes, components, and external required modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { AppComponent } from './home/app.component';
+import { ProgamerItemsComponent } from './progamer-items/progamer-items.component';
+
+import { ObjectIteratorPipe } from './shared/pipes/object-iterator.pipe';
+import { SearchPipe } from './shared/pipes/search.pipe';
+import { ProgamerDetailsComponent } from './progamer-details/progamer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectIteratorPipe,
+    ProgamerItemsComponent,
+    SearchPipe,
+    ProgamerDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
